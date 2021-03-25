4297ab585fa0c738eb1625c429d5c1224f6313d1
hashim m <hashim.muqtadir@gmail.com>
2021-03-25 14:15:45 +1100

    Added some property based testing. This work was done after the time limit.

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ One may use either the object directly, or the functional API which wraps over t
 
 The API provides functions to create a scorecard, upon which the rest of the API acts; "throw", i.e. record pins being knocked by rolling the ball; get the total score so far; get the score for a given frame number (1-indexed), and to see if the scorecard is complete.
 
-Tests are in test_bowling.py and may be run by invoking `python -m unittest` from the project root directory. (Or by running `python test_bowling.py`).
+Tests are in test_bowling.py and may be run by invoking `python -m unittest` from the project root directory. (Or by running `python test_bowling.py`). Property-based testing included.
 
 Effort was made to keep the code readable. It is not "optimized" much.
--- test_bowling.py
+++ test_bowling.py
@@ -1,12 +1,38 @@
 import bowling
 import unittest
+from hypothesis import example, given, strategies as st
+from itertools import chain
+
+strike_st = lambda: st.just([10])
+spare_st = lambda: st.one_of(
+    list(map(lambda x: st.just(list(x)), zip(range(10), range(10, 0, -1))))
+)
+
+@st.composite
+def normal_st(draw):
+    first_shot = draw(st.integers(0, 9))
+    second_shot = draw(st.integers(0, 9 - first_shot))
+    return [first_shot, second_shot]
+
+frame_st = lambda: st.one_of(strike_st(), spare_st(), normal_st())
+
+tenth_frame_st = lambda: st.one_of(
+    normal_st(),
+    st.builds(lambda x, y: x + [y], spare_st(), st.integers(0, 10)),
+    st.builds(lambda x, y: [10, x, y], st.integers(0, 10), st.integers(0, 10)),
+)
+
+def combine_shots(first_9, tenth):
+    return list(chain.from_iterable([*first_9, tenth]))
+
+all_shots_st = lambda: st.builds(combine_shots, st.lists(frame_st(), min_size=9, max_size=9), tenth_frame_st())
 
 
 class BowlingTestCase(unittest.TestCase):
     def test_create_scorecard(self):
         scorecard = bowling.create_scorecard()
         assert isinstance(scorecard, bowling.Scorecard)
-    
+
     def test_frame_filling_strikes_spares(self):
         scorecard = bowling.create_scorecard()
         scorecard.throw(1)
@@ -28,7 +54,7 @@ class BowlingTestCase(unittest.TestCase):
         self.assertEqual(scorecard.frames[3], bowling.Frame(shots=[5]))
         self.assertFalse(scorecard.frames[3].is_strike)
         self.assertFalse(scorecard.frames[3].is_spare)
-    
+
     def test_final_frame(self):
         scorecard = bowling.create_scorecard()
         shots = [10, 7, 3, 7, 2, 9, 1, 10, 10, 10, 2, 3, 6, 4, 7, 2]
@@ -65,8 +91,38 @@ class BowlingTestCase(unittest.TestCase):
         self.assertRaises(AssertionError, lambda: bowling.throw(scorecard, 5))
         for idx, test_score in enumerate([20, 17, 9, 20, 30, 22, 15, 5, 17, 13]):
             frame_number = idx + 1
-            self.assertEqual(bowling.score_for_frame(scorecard, frame_number), test_score)
+            self.assertEqual(
+                bowling.score_for_frame(scorecard, frame_number), test_score
+            )
         self.assertEqual(bowling.total_score(scorecard), 168)
+
+    # THIS TEST CAN EFFECTIVELY REPLACE ALL THE OTHERS EXCEPT THE ONES THAT CHECK TOTAL SCORE
+    @given(all_shots_st())
+    def test_properties_shots(self, shots):
+        scorecard = bowling.create_scorecard()
+        for score in shots:
+            scorecard = bowling.throw(scorecard, score)
+        self.assertTrue(scorecard.is_complete)
+        self.assertEqual(len(scorecard.frames), 10)
+        for frame in scorecard.frames[:-1]:
+            assert frame.shots
+            if frame.shots[0] == 10:
+                self.assertEqual(len(frame.shots), 1)
+            else:
+                self.assertEqual(len(frame.shots), 2)
+                assert sum(frame.shots) <= 10
+        last_frame = scorecard.frames[-1]
+        assert len(last_frame.shots) >= 2
+        for shot in last_frame.shots:
+            assert shot <= 10
+        if last_frame.shots[0] == 10:
+            self.assertEqual(len(last_frame.shots), 3)
+        elif (last_frame.shots[0] + last_frame.shots[1]) == 10:
+            self.assertEqual(len(last_frame.shots), 3)
+        else:
+            self.assertEqual(len(last_frame.shots), 2)
+        # TODO: SOME TEST FOR TOTAL SCORE
+
 
 
 if __name__ == "__main__":
